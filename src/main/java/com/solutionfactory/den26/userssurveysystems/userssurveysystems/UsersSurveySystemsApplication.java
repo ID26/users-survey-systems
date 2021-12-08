e95c2e1eaebfb468b4ffa9e8a69b40fb5eb54c9d
Ваше Имя <you@example.com>
2021-12-08 12:59:42 +0300

    first commit

---
+++ src/main/java/com/solutionfactory/den26/userssurveysystems/userssurveysystems/UsersSurveySystemsApplication.java
@@ -0,0 +1,13 @@
+package com.solutionfactory.den26.userssurveysystems.userssurveysystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersSurveySystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersSurveySystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solutionfactory/den26/userssurveysystems/userssurveysystems/UsersSurveySystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutionfactory.den26.userssurveysystems.userssurveysystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersSurveySystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
